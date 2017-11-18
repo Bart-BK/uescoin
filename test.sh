1f5d1cf25a7c79205850e4ac90f31f7b046b1179
Bart-BK <bartinhow@live.com>
2017-11-17 23:15:43 -0300

    Update

--- test.sh
+++ test.sh
@@ -1,4 +1,22 @@
-clear
-python3 /home/local3/adonias/Workspace/Python/Blockchain/testServer.py &
-sleep 2
-python3 /home/local3/adonias/Workspace/Python/Blockchain/testClient.py
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+if [[ ${machine} == "MinGw" ]]; then
+	#echo "MinGw"
+	clear
+	py $(pwd)/testServer.py &
+	sleep 2
+	py $(pwd)/testClient.py
+elif [[ ${machine} == "Linux" ]]; then
+	#echo "Linux"
+	clear
+	python3 $(pwd)/testServer.py &
+	sleep 2
+	python3 $(pwd)/testClient.py
+
+fi
