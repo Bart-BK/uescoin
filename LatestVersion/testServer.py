986a670fdd6c2ffe70adf116744dd0b8d7855f72
adoniasalcantara <adonias.neto@outlook.com>
2018-01-12 11:56:15 -0300

    Atualização

--- LatestVersion/testServer.py
+++
@@ -1,7 +0,0 @@
-from server import ServerListener
-from transaction import TransactionDispatcher
-
-"""
-	Inicia o listener com uma sub-classe de Dispatcher
-"""
-ServerListener(TransactionDispatcher).start()
--- testServer.py
+++
@@ -1,3 +0,0 @@
-from server import ServerListener
-
-ServerListener().start()
